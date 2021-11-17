257940812468b6f2686cd75123ef03d305d1f36d
jhdcruz <jhdcrux@outlook.com>
2021-11-17 22:25:23 +0800

    fix incompatible type input

--- src/js/order.js
+++ src/js/order.js
@@ -12,7 +12,7 @@ const cappuccino = 60.0;
 
 // Order ID Generator
 let id = Math.floor(100000000 + Math.random() * 900000000);
-let receipt_id = (d.querySelector("#order_id").innerHTML = id);
+let receipt_id = (d.querySelector("#order_id").innerHTML = `${id}`);
 
 // --------------------- SELECTORS -------------------------
 
