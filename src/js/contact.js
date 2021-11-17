8b2a7df43858ab7a304a7002b530d56ed73183f3
jhdcruz <jhdcrux@outlook.com>
2021-11-18 00:46:33 +0800

    dynamic contact page

---
+++ src/js/contact.js
@@ -0,0 +1,23 @@
+function generateTicket() {
+  // Dos[;ay ticket on form submit
+  let ticket = document.querySelector("#ticket");
+  let contact = document.querySelector("#contact-form");
+  ticket.style.display = "block";
+  contact.style.display = "none";
+
+  // Ticket number generator
+  let id = Math.floor(10000 + Math.random() * 90000);
+  document.querySelector("#ticket_no").innerHTML = `${id}`;
+
+  // Display form inputs
+  const inputName = document.querySelector("#fn").value;
+  document.querySelector("#sentName").innerHTML = `${inputName}`;
+
+  const inputEmail = document.querySelector("#email").value;
+  document.querySelector("#sentEmail").innerHTML = `${inputEmail}`;
+
+  const inputMessage = document.querySelector("#message").value;
+  document.querySelector("#sentMessage").innerHTML = `${inputMessage}`;
+
+  return false;
+}
